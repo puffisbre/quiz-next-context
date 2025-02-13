05d16e814d5fc0717dd87850b2ad080e5992911f
Felix.O.S <puffis.stenersjo@gmail.com>
2025-02-13 14:17:14 +0100

    heaweaweiaew

--- app/run-quiz/page.tsx
+++ app/run-quiz/page.tsx
@@ -1,3 +1,25 @@
+'use client'
+
+import { QuizContext } from "../context/QuizContext";
+import React, { useContext, useEffect } from "react";
+
+
 export default function RunQuiz() {
-  return <></>;
+  const context = useContext(QuizContext);
+    const questions = context?.questions ?? [];
+  //  const setQuestions = context?.setQuestions ?? (() => {});
+  useEffect(() => {
+    console.log(questions);
+  }, [questions]);
+  return (
+  <>
+  <div>
+        {questions.map((q, index) => (
+          <div key={index}>
+            <p>Question: {q.question}</p>
+            <p>Answer: {q.answer}</p>
+          </div>
+        ))}
+      </div>
+  </>);
 }
